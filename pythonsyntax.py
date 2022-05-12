8dd9a021e6e6e4837a0cd8a672aa67c90b91ae01
DavidSaruni <sarunidavid11126@gmail.com>
2022-05-12 13:25:56 +0300

    Python Syntax

---
+++ pythonsyntax.py
@@ -0,0 +1,11 @@
+# This is the python syntax
+
+print("Hello, Captain")
+
+name = 'Captain David'
+print("Hello", name)
+
+msg = 'Congratulations'
+msg2 = 'You have made the greatest step in your life'
+
+print(msg, name, msg2)
